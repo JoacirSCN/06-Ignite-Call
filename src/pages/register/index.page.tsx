b24cdddca2201d2b69d7b2cba6bbd5e9a549f6ec
joacir <joacirprog@gmail.com>
2023-05-06 17:08:32 -0300

    Persistindo usuários no Prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -5,8 +5,10 @@ import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { api } from '../../lib/axios'
 
 import { Container, Form, FormError, Header } from './styles'
+
 const registerFormSchema = z.object({
   username: z
     .string()
@@ -29,18 +31,24 @@ export default function Register() {
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
   })
-
   const router = useRouter()
-
   useEffect(() => {
     if (router.query.username) {
       setValue('username', String(router.query.username))
     }
   }, [router.query?.username, setValue])
 
-  function handleRegister(data: RegisterFormData) {
-    console.log(data)
+  async function handleRegister(data: RegisterFormData) {
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
+
   return (
     <Container>
       <Header>
